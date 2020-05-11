f3a8ae1920fe2e966952c7156d2a488e6689a6a4
guayama.agustin <agustin.rta@gmail.com>
2020-05-11 15:22:08 -0300

    Web app running on the happy paths. Needs work on Error handling.

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, flash, request
+from wtforms import Form, StringField, PasswordField
+from wtforms.validators import EqualTo, InputRequired, Length
+from devices import all_devices
+import secrets
+
+
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
+
+
+class ReusableForm(Form):
+    username = StringField('Name:', validators=[InputRequired(), Length(min=6, max=35)])
+    password = PasswordField('Password:', validators=[InputRequired(), Length(min=3)])
+    confirm_password = PasswordField('confirm_password:', validators=[EqualTo('password')])
+
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    form = ReusableForm(request.form)
+    if request.method == 'POST' and form.validate():
+        mk_user = form.username.data
+        mk_pass = form.password.data
+        print('Username: ', mk_user)
+        print('Password: ', mk_pass)
+        flash('Initiating router connections for ' + mk_user)
+        all_devices(input_username=mk_user, input_password=mk_pass)
+        print('Done')
+    else:
+        flash('Error: Check the passwords')
+    return render_template('index.html', form=form)
+
+if __name__ == "__main__":
+    app.run()
---
+++ devices.py
@@ -0,0 +1,126 @@
+import csv
+import ssl
+import sys
+import time
+import librouteros
+from librouteros.query import Key
+import json
+# import pdb
+#pdb.set_trace()
+
+# Admin user importing
+
+# with open('config.json') as config_file:
+#     data = json.load(config_file)
+# mk_admin_user = data['admin_user']
+# mk_admin_pw = data['admin_pw']
+timestr = time.strftime("%Y%m%d-%H%M%S")
+report = {}
+
+# Username and password wich is going to be created, or whose password is going to be updated
+#TODO: work on username and password input for standalone instances
+username = 'hh4444444'
+userpass = '344444433'
+
+
+def all_devices(input_username=None,input_password=None):
+    conf = get_conf()
+
+    for line in conf['input_file']:
+        ipmk = line.get("Addresses")
+        namemk = line.get("Name")
+        if __name__ == "__main__": # If runs as standalone, you get real time report of what's being procesed
+            aux_my_print('Procesando equipo:\t\t\t' + namemk + '\n')
+        router = Device(ipmk, namemk, input_username, input_password)
+        router.createusers()
+
+def get_conf(): # look for config in config file
+    conf_dict = {}
+    with open('config.json') as config_file:
+        data = json.load(config_file)
+    conf_dict['mk_admin_user'] = data['admin_user']
+    conf_dict['mk_admin_pw'] = data['admin_pw']
+
+    if __name__ == "__main__":  # If runs as standalone, ask for args
+        lista_equipos = args_parser()
+    else:
+        lista_equipos = data['devices-file']
+    conf_dict['input_file'] = csv.DictReader(open(lista_equipos))
+    return conf_dict
+
+
+class Device:
+    def __init__(self, ip, device_name, input_username,input_password):
+        if not input_password: #if the password comes empty
+            return
+        self.device_name = device_name
+        self.ip = ip
+        self.input_username = input_username
+        self.input_password = input_password
+
+    def connect(self):
+        conf = get_conf()
+        try:
+            ssl_layer = ssl.create_default_context()
+            ssl_layer.check_hostname = False
+            ssl_layer.set_ciphers('ADH:@SECLEVEL=0')
+            mkapi = librouteros.connect(
+                username = conf['mk_admin_user'],
+                password = conf['mk_admin_pw'],
+                host=self.ip,
+                ssl_wrapper=ssl_layer.wrap_socket,
+                port=8729
+            )
+        except Exception as err:
+            print('Problem connecting to ' + self.device_name + '. Error was: ' + str(err))
+            return err
+        return mkapi
+
+    def createusers(self):
+        try:
+            device = self.connect()
+            create_params = {
+                'name': self.input_username,
+                'password': self.input_password,
+                'group': 'full' #TODO: manage group configuration
+            }
+            users = device.path('/user')
+            mkusername = Key('name')
+            item_id = Key('.id')
+            user_exists = False
+            # TODO: create function to get .id me clearly
+            for row in users.select(item_id, mkusername):
+                if self.input_username == row['name']:
+                    user_exists = True
+                    update_params = {'password': self.input_password, '.id': row['.id']}
+                    users.update(**update_params)
+                    print('Password updated: ' + row['name'] + ' with pass: ' + self.input_password + ' in device: ' + self.device_name)
+
+            if user_exists is False:
+                device.path('/user').add(**create_params)
+                print('User ' + self.input_username + ' created with password ' + self.input_password + ' in device: ' + self.device_name)
+
+        except Exception as err:
+            print('Problem with ' + self.device_name + '. Error msj: ' + str(err))
+            return err
+        return
+
+def args_parser():
+    if len(sys.argv) == 1:
+        print('Use \'-in filename.csv\' for data...')
+        sys.exit()
+    elif (sys.argv[1]).lower() == '-in':
+        lista_equipos = sys.argv[2]
+        return lista_equipos
+    else:
+        print('Parameters problem')
+        sys.exit()
+
+def aux_my_print(text):
+    sys.stdout.write((str(text))+'                          \r')
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    all_devices()
+
