4d289be398a8029bb53d3bc1265f7004af1b05bf
guayama.agustin <agustin.rta@gmail.com>
2020-05-21 16:54:04 -0300

    firsts tries at celery

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from wtforms import Form, StringField, PasswordField
 from wtforms.validators import EqualTo, InputRequired, Length
 from devices import all_devices
 import secrets
+from flask_celery import make_celery
 
 
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-
+celery = make_celery(app)
 class ReusableForm(Form):
     username = StringField('Name:', validators=[InputRequired(), Length(min=6, max=35)])
     password = PasswordField('Password:', validators=[InputRequired(), Length(min=3)])
@@ -23,14 +26,23 @@ def user_create():
     if request.method == 'POST' and form.validate():
         mk_user = form.username.data
         mk_pass = form.password.data
-        print('Username: ', mk_user)
-        print('Password: ', mk_pass)
+        #print('Username: ', mk_user)
+        #print('Password: ', mk_pass)
         flash('Initiating router connections for ' + mk_user)
-        all_devices(input_username=mk_user, input_password=mk_pass)
+        call_routers(input_username=mk_user, input_password=mk_pass)
         print('Done.')
     else:
         flash('Error: Check the passwords')
+        #print(form.username.data)
+        #print(form.password.data)
+        #print(form.confirm_password.data)
     return render_template('index.html', form=form)
+
+
+@celery.task(name='app.call_routers')
+def call_routers():
+    return all_devices(mk_user, mk_pass)
+
 
 if __name__ == "__main__":
     app.run()
